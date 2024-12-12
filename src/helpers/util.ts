9f69634fa89b9bfa945432ae4de2a46b45c8774b
Lương Cao Chấn Huy <161787746+LuongCaoChanHuyIT@users.noreply.github.com>
2024-12-12 08:32:25 +0700

    update crud pagination

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -1,5 +1,5 @@
-import bcrypt from 'bcrypt'
-const saltRounds = 100
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 export const hashPasswordHelper = async (plainPassword: string) => {
   try {
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,22 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Model } from 'mongoose';
 import { User } from './schemas/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { hashPasswordHelper } from '@/helpers/util';
-
+import aqp from 'api-query-params';
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  async create(createUserDto: CreateUserDto) {
-    const hashPassword = await hashPasswordHelper(createUserDto.password)
-    return 'This action adds a new user';
+  isEmailExist = async (email: string) => {
+    const user = await this.userModel.exists({email})
+    if(user) return true
+    return false
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async create(createUserDto: CreateUserDto) {
+    const {name, email, password} = createUserDto
+
+    const isExist = this.isEmailExist(email)
+    if(isExist){
+      throw new BadRequestException(`Email is exist ${email}`)
+    }
+
+    const hashPassword = await hashPasswordHelper(password)
+    const user = await this.userModel.create({
+      name, email, password: hashPassword
+    })
+    return {
+      _id: user._id
+    }
+  }
+
+  async findAll(query: string, current: number, pageSize: number) {
+    const {filter, sort} = aqp(query)
+    if(filter.current) delete filter.current
+    if(filter.pageSize) delete filter.pageSize
+    if(!current) current = 1
+    if(!pageSize) pageSize = 10
+    const totalItems = (await this.userModel.find(filter)).length
+    const totalPages = Math.ceil(totalItems/pageSize)
+    const skip = (current - 1) * pageSize
+    const results = await this.userModel.find(filter).limit(pageSize).skip(skip).select("-password").sort(sort as any)
+    return {results, totalPages};
   }
 
   findOne(id: number) {
